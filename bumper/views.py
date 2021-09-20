2991c3bcebd1db84ebadbbe5a9a5ee361a7aae72
Miguel R <contact@migue.pro>
2021-09-20 11:13:51 -0400

    images in greyscale

--- bumper/views.py
+++ bumper/views.py
@@ -4,16 +4,13 @@ from .models import Product, ProductImage
 
 # Create your views here.
 def index(request):
-    last_bike = Product.objects.all().order_by('-date')[0]
-    last_bike_picture = ProductImage.objects.filter(product=last_bike)
-    # last_bike_picture = ProductImage.objects.all().order_by('-date')[0]
-    last_bike_picture = ProductImage.objects.filter(product=last_bike)
-    rowers = Product.objects.filter(category='ROWER')
-
-    barbells = Product.objects.filter(category='BARBELL')
-    bumpers = Product.objects.filter(category='BUMPER')
-    general = Product.objects.filter(category='GENERAL')
-    return render(request, 'index.html', {'last_bike': last_bike, "last_bike_picture": last_bike_picture })
+    last_product = Product.objects.all().order_by('-date')[0]
+    last_picture = ProductImage.objects.filter(product=last_product)
+    # rowers = Product.objects.filter(category='ROWER')
+    # barbells = Product.objects.filter(category='BARBELL')
+    # bumpers = Product.objects.filter(category='BUMPER')
+    # general = Product.objects.filter(category='GENERAL')
+    return render(request, 'index.html', {'last': last_product, "last_picture": last_picture[0] })
 
 
 def categories(request, category):
